c76a223f5911835c96dedbdc5710be1439dbc515
vipul0999 <kalevipul12@gmail.com>
2025-03-26 15:53:32 -0500

    Added new endpoint /mydata

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var mydataRouter = require('./routes/mydata');
 
 var app = express();
 
@@ -15,6 +16,7 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use('/mydata', mydataRouter);
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
---
+++ routes/mydata.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET mydata page. */
+router.get('/', function(req, res, next) {
+  res.render('mydata', { title: 'Your Full Name' });
+});
+
+module.exports = router;
+
