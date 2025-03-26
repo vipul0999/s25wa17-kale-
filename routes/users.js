fe0d70fd637c6b07365c2ee0b9772a525a00fa3d
vipul0999 <kalevipul12@gmail.com>
2025-03-26 16:41:11 -0500

    new changes made

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+let ratio = 0.75;
+let acc = 1000;
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  acc *= ratio;  
+  res.send(`Value is: ${acc}`);
 });
 
 module.exports = router;
